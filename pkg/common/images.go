6a79e0c959158a35dd8018b16e875a7957d89be9
Zelmar Michelini <zelmar@michelini.com.uy>
2026-08-21 16:12:18 -0300

    release prep: bump version constants to 0.1.39
    ANSIBLE_VERSION, IMAGE_VERSION and the general_version anydbver row all move
    together. The SQL row is what ReadDatabaseVersion returns, so leaving it behind
    makes every up-to-date user see the version-database nag on every command.

--- pkg/common/images.go
+++ pkg/common/images.go
@@ -8,7 +8,7 @@ const (
 )
 
 var RELEASE_VERSION = "0.1.23"
-var ANSIBLE_VERSION = "0.1.38"
+var ANSIBLE_VERSION = "0.1.39"
 
 func GetDockerImageName(osver string, user string) string {
 	platform_tag := ""
